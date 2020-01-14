345720c626c2b36ff4a76305d6c1b953d72bcbf1
Luciano Medeiros Marcelino <lucianom@nulogy.com>
2020-01-14 12:31:46 -0500

    First commit

---
+++ crawler.rb
@@ -0,0 +1,59 @@
+require 'capybara/dsl'
+# require 'byebug'
+
+Capybara.run_server = false
+Capybara.current_driver = :selenium
+Capybara.app_host = "https://www.ontario.ca/page/2020-ontario-immigrant-nominee-program-updates"
+
+module MyCapybara
+  class Crawler
+    include Capybara::DSL
+
+    def read_page_body
+      visit("/")
+
+      find("#pagebody").text
+    end
+  end
+end
+
+def log_start
+  File.open("run_logs.txt", "a") do |file|
+    file.write("Crawling started at #{Time.now}. ")
+  end
+end
+
+def log_end(updated)
+  File.open("run_logs.txt", "a") do |file|
+    file.write("Finished at #{Time.now}.")
+    if updated
+      file.write(" The OINP has a new update.\n")
+    else
+      file.write(" The OINP has no new updates.\n")
+    end
+  end
+end
+
+def run
+  log_start
+  crawler = MyCapybara::Crawler.new
+  pagebody = crawler.read_page_body
+  previous_pagebody = File.read("pagebody.txt")
+  updated = pagebody != previous_pagebody
+  if updated
+    # Save the older pagebody just for debugging purposes
+    File.open("debug_older_pagebody.txt", "w") do |file|
+      file.write("#{previous_pagebody}")
+    end
+
+    # Updates the file with the current pagebody
+    File.open("pagebody.txt", "w") do |file|
+      file.write("#{pagebody}")
+    end
+
+    # TODO: SEND EMAIL TO ME AND CARLA
+  end
+  log_end(updated)
+end
+
+run
