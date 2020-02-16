adb4f25826cf6ee42d0f223471b214bb1d8b77e1
Luciano Medeiros Marcelino <lucianom@nulogy.com>
2020-02-15 20:37:21 -0500

    Set up git action to run tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,7 @@ require_relative "../custom_logger.rb"
 require_relative "../file_handler.rb"
 require_relative "../vcr_setup"
 require "webmock/rspec"
+
+ENV["AWS_REGION"] = "us-east-1"
+ENV["AWS_ACCESS_KEY_ID"] = "AKabcde"
+ENV["AWS_SECRET_ACCESS_KEY"] = "abcde"
