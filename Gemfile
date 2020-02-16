781313473a1470611731e26db56293fc65102e26
Luciano Medeiros Marcelino <lucianom@nulogy.com>
2020-02-15 20:52:42 -0500

    Add code coverage configs

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem "tlsmail"
 
 group :test do
   gem "rspec", "~> 3.9"
+  gem "simplecov", require: false
   gem "timecop"
   gem "vcr", "~> 5.1"
   gem "webmock"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
+require "simplecov"
+SimpleCov.start do
+  track_files '**/*.rb'
+end
+
 require_relative "../custom_logger.rb"
 require_relative "../file_handler.rb"
 require_relative "../vcr_setup"
