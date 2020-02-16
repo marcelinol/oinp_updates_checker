7d77b92dae3f4d26be0502a8468ef58bb82457d1
Luciano Medeiros Marcelino <lucianom@nulogy.com>
2020-02-16 08:50:14 -0500

    Use new method instead of delete constant for local paths

--- crawler.rb
+++ crawler.rb
@@ -21,7 +21,7 @@ class Crawler
     pagebody = read_page_body
 
     @file_handler.download_saved_pagebody
-    previous_pagebody = File.open(FileHandler::PAGEBODY_LOCAL_PATH, "r:UTF-8", &:read)
+    previous_pagebody = File.open(@file_handler.local_path(filename: FileHandler::PAGEBODY_FILENAME), "r:UTF-8", &:read)
     pagebody_changed = pagebody != previous_pagebody
 
     if pagebody_changed
